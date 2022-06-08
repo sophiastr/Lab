d04575653209ab72bbacfacdf2b372fe93fb0a84
Σοφία <sophiastr01@hotmail.com>
2022-06-08 03:04:15 +0300

    Add tests to check ArithmeticOperations class #4

--- arithmeticoperations/src/test/java/math/ArithmeticOperationsTest.java
+++ arithmeticoperations/src/test/java/math/ArithmeticOperationsTest.java
@@ -15,6 +15,37 @@ public class ArithmeticOperationsTest {
 	}
 	
 	@Test
+	public void test_divide_0dividedby2() {
+		Assert.assertEquals(0 , ar.divide(0, 2), 0.001);
+	}
+	
+	@Test
+	public void test_divide_1dividedby8() {
+		Assert.assertEquals(0.125 , ar.divide(1, 8), 0.001);
+	}
+	
+	@Test
+	public void test_divide_10dividedbynegative() {
+		Assert.assertEquals(-1 , ar.divide(10, -10), 0.001);
+	}
+	
+	@Test
+	public void test_divide_10dividedby3() {
+		Assert.assertEquals(3.333 , ar.divide(10, 3), 0.001);
+	}
+	
+	@Test
+	public void test_divide_negativedividedby3() {
+		Assert.assertEquals(0 , ar.divide(-0, 3), 0.001);
+	}
+	
+
+	@Test
+	public void test_divide_two_negatives() {
+		Assert.assertEquals(9 , ar.divide(-27, -3), 0.001);
+	}
+	
+	@Test
 	public void test_divide_3dividedby3() {
 		Assert.assertEquals(1. , ar.divide(3, 3), 0.001);
 	}
@@ -29,6 +60,11 @@ public class ArithmeticOperationsTest {
 		Assert.assertEquals(8, ar.multiply(2, 4));
 	}
 	
+	@Test
+	public void test_multiply_multiplied1by1() {
+		Assert.assertEquals(1, ar.multiply(1, 1));
+	}
+	
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 	
@@ -37,6 +73,13 @@ public class ArithmeticOperationsTest {
 		 thrown.expect(IllegalArgumentException.class);
 		 thrown.expectMessage("x & y should be >= 0");
 		 ar.multiply(-1, 2);
+	}
+	
+	@Test
+	public void test_multiply_negative() {
+		 thrown.expect(IllegalArgumentException.class);
+		 thrown.expectMessage("x & y should be >= 0");
+		 ar.multiply(1, -2);
 	}
 	
 	@Test
@@ -51,6 +94,20 @@ public class ArithmeticOperationsTest {
 		 thrown.expect(IllegalArgumentException.class);
 		 thrown.expectMessage("The product does not fit in an Integer variable");
 		 ar.multiply((Integer.MAX_VALUE/2) + 1,  Integer.MAX_VALUE/2);
+	}
+	
+	@Test
+	public void test_multiply_not_integer_example2() {
+		 thrown.expect(IllegalArgumentException.class);
+		 thrown.expectMessage("The product does not fit in an Integer variable");
+		 ar.multiply((Integer.MAX_VALUE/5) + 1,  Integer.MAX_VALUE/5);
+	}
+	
+	@Test
+	public void test_multiply_not_integer_example3() {
+		 thrown.expect(IllegalArgumentException.class);
+		 thrown.expectMessage("The product does not fit in an Integer variable");
+		 ar.multiply(Integer.MAX_VALUE/5, 10);
 	}
 
 	
