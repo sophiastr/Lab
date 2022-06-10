e7ee09f102beb26cab48b44c410617d5b32e9bd8
Σοφία <sophiastr01@hotmail.com>
2022-06-10 16:15:17 +0300

    Add test to check ArrayOperations class #6

--- arithmeticoperations/src/test/java/math/ArrayOperationsTest.java
+++ arithmeticoperations/src/test/java/math/ArrayOperationsTest.java
@@ -12,18 +12,25 @@ public class ArrayOperationsTest {
 
 	ArrayOperations ao = new ArrayOperations();
 	FileIO fileio = new FileIO();
+	
 	@Test
 	public void test_prime_array_Mocking() {
+
+		String filepath = "src/test/resources/primenumbers";
+		FileIO fio = mock(FileIO.class);
+		 when(fio.readFile(filepath)).thenReturn(new int[]
+			      {-10, -2, -1, 0, 1, 2, 3, 4, 5, 6});
+		
 		MyMath m = mock(MyMath.class);
+		
 		when(m.isPrime(2)).thenReturn(true);
 		when(m.isPrime(3)).thenReturn(true);
 		when(m.isPrime(4)).thenReturn(false);
 		when(m.isPrime(5)).thenReturn(true);
 		when(m.isPrime(6)).thenReturn(false);
-
-		int[] testvalues = {2, 3, 4, 5, 6};
 		
-		Assert.assertArrayEquals(new int[] {2, 3, 5}, ao.findPrimesInFile(fileio, "src/test/resources/primenumbers", m));
+		
+		Assert.assertArrayEquals(new int[] {2, 3, 5}, ao.findPrimesInFile(fileio, filepath, m));
 	}
 
 }
